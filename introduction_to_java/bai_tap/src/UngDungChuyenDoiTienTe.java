2fb234e76b565ffa1519bfbee1479263cfb61ed3
unknown <buivanluu1997@gmail.com>
2024-09-09 08:56:29 +0700

    Nop bai tap

---
+++ introduction_to_java/bai_tap/src/UngDungChuyenDoiTienTe.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UngDungChuyenDoiTienTe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double vnd = 23000;
+        double quyDoi;
+        System.out.println("Nhập giá trị USD: ");
+        double usd = scanner.nextInt();
+        quyDoi = usd * vnd;
+        System.out.println(usd + " USD" + " = " + quyDoi + " VND");
+    }
+}
